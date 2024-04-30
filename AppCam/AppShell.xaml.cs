c031b60913a0897116bda73a9951b5dc78486928
ant7tv@gmail.com <ant7tv@gmail.com>
2024-04-30 15:22:50 +0200

    Добавьте файлы проекта.

---
+++ AppCam/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppCam
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppCam/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppCam
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
